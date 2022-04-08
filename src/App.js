978694b316139e8a31b3cf00b055277b39e02a82
Racknar0 <racknarow1@gmail.com>
2022-04-08 00:38:33 -0500

    Configurar Auuth v1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useReducer,useEffect } from 'react'
 import LoginRouter from './routes/LoginRouter'
+import { AuthContext } from './context/AuthContext'
+import { AuthReducer } from './reducers/AuthReducer'
 
+
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('log')) || {log: false}
+}
 
 const App = () => {
+
+  const [log, dispatch] = useReducer(AuthReducer, {}, third)
+
+  useEffect(() => {
+    localStorage.setItem('log', JSON.stringify(log))
+  }, [log])
+  
+
   return (
     <LoginRouter />
   )
