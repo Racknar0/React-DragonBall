02e7679c166ca0638edcfbc438598f4440c5ce09
Racknar0 <racknarow1@gmail.com>
2022-04-07 23:07:15 -0500

    Add Woman Screen

--- src/pages/CharacterScreen.jsx
+++ src/pages/CharacterScreen.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router'
+import { Characters } from '../models/Characters'
 
-const CharacterScreen = () => {
+const CharacterScreen = ({history}) => {
 
     const {id} = useParams()
-    
+    const {type, description, name} = Characters.find(character => character.id === id)
+    const path = `/assets/${type}-${id}.png`
 
-
+    const handleBack = () => {
+        history.goBack()
+    }
   return (
-    <div>
-        {id}
+    <div className='container row mt-5'>
+        <div className='col-8 d-flex justify-content-center'>
+            <img className='img-thumbnail mt-3 img-fluid ' src={path} alt={id} style={{
+                /* width: '100%', */
+                maxHeight: '400px'
+            }} />
+        </div>
+        <div className='col-4'>
+            <h2>Nombre: {name}</h2>
+            <p>Descripción: {description}</p>
+            <button className='btn btn-outline-danger' onClick={handleBack}>Go Back</button>
+        </div>
     </div>
   )
 }
